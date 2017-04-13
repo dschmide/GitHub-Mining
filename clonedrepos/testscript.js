5058506047cbe9c9203791417f96af80ce0b8549
dschmide <daniel.schmider@hsr.ch>
2017-04-13 03:14:27 +0200

    deleted unnecessary files

---
+++ clonedrepos/testscript.js
@@ -0,0 +1,26 @@
+//NewExpressions
+var test = new object();
+var test1 = new object();
+
+new object();
+
+var mycar = new Car('Eagle', 'Talon TSi', 1993);
+var rand = new Person('Rand McNally', 33, 'M');
+var ken = new Person('Ken Jones', 39, 'M');
+
+var obj = New(A, 1, 2);
+var obj = new A(1, 2);
+
+//EmptyStatements
+;
+;
+{
+	;
+	nothing;
+	;
+}
+;
+
+var myCar = Object.create();
+var myCar = {};
+var myCar = Object.destroy();
